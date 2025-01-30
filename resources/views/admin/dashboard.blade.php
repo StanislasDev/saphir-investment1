5a351b0af3c1b9b750e0c2a4439fc3cf4115e0aa
Stanislas <simostanley91@gmail.com>
2025-01-30 13:27:51 +0100

    Ajout de nouveaux fichiers SCSS pour les helpers, mixins et styles de Bootstrap, ainsi que des configurations pour Font Awesome. Et finalisation du dashboard pour l'admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,34 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.nav')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+@section('title', 'Tableau de bord Admin')
+
+@section('header')
+<x-app-header-layout/>
+@endsection
+
+@section('content')
+vibibzibvbivbzibib
+    <!-- Bouton pour ouvrir la modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Ouvrir la modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Titre de la Modal</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Contenu de la modal.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                    <button type="button" class="btn btn-primary">Enregistrer</button>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="fr" class="h-full bg-gray-50">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="Portal - Bootstrap 5 Admin Dashboard Template For Developers">
+    <link rel="shortcut icon" href="{{ asset('assets/images/ESCa-final-usa-fond-noir-1-1024x753.ico') }}">
+
+    <link href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- FontAwesome JS -->
+    <script defer src="{{ asset('assets/plugins/fontawesome/js/all.min.js') }}"></script>
+
+    <!-- App CSS -->
+    <link id="theme-style" rel="stylesheet" href="{{ asset('assets/css/portal.css') }}">
+
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="app">
+    @if (session('status'))
+        <div class="app-header fixed-top">
+            <div class="flex">
+                <div class="flex-shrink-0">
+                    <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </div>
+                <div class="ml-3">
+                    <p class="text-sm font-medium text-green-800">{{ session('status') }}</p>
+                </div>
+            </div>
+        </div>
+    @endif
+
+    <body class="app">
+        <header class="app-header fixed-top h-16">
+            @yield('header')
+            <x-side-bar />
+        </header><!--//app-header-->
+
+            {{-- {{ $slot }} --}}
+            <div class="app-wrapper">
+
+                <div class="app-content pt-3 p-md-3 p-lg-4">
+                    @yield('content')
+                </div>
+            </div>
+
+        </div><!--//app-wrapper-->
+
+
+        <!-- Javascript -->
+        <script src="{{ asset('assets/plugins/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+
+        <!-- Charts JS -->
+        <script src="{{ asset('assets/plugins/chart.js/chart.min.js') }}"></script>
+        <script src="{{ asset('assets/js/index-charts.js') }}"></script>
+
+        <!-- Page Specific JS -->
+        <script src="{{ asset('assets/js/app.js') }}"></script>
+
+    </body>
+
+</html>
+
+
+</body>
+
+</html>
